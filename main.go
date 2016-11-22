c978fe55c4ec20a453ac67a58fe1360ff08c5503
Ho Yan Leung <hy.leung@gmail.com>
2016-11-22 06:55:01 -0800

    Get health for single container

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/engine-api/client"
 	cli "github.com/urfave/cli"
+	"golang.org/x/net/context"
 	"os"
 )
 
@@ -18,14 +20,43 @@ func main() {
 	app.Name = "docker-health"
 	app.Usage = "Docker healthcheck utility"
 	app.Version = "1.0"
-	app.Action = func(c *cli.Context) error {
-		defaultHeaders := map[string]string{"User-Agent": user_agent}
-		_, client_err := client.NewClient("unix:///var/run/docker.sock", docker_api_version, nil, defaultHeaders)
-		if client_err != nil {
-			panic(client_err)
-		}
-		log.Info("Connected to Docker daemon...")
-		return nil
+	app.Commands = []cli.Command{
+		{
+			Name:  "inspect",
+			Usage: "Inspect the Health Check status of a container",
+			Action: func(c *cli.Context) error {
+				docker_client, client_err := createClient()
+				if client_err != nil {
+					panic(client_err)
+				}
+				log.Info("Connected to Docker daemon...")
+				containerName := c.Args().First()
+				log.Infof("Getting health for %s", containerName)
+				containerJson, err := docker_client.ContainerInspect(context.Background(), containerName)
+				if err != nil {
+					if client.IsErrContainerNotFound(err) {
+						fmt.Printf("Container '%s' not found", containerName)
+						return nil
+					} else {
+						panic(err)
+					}
+				}
+				log.Info(containerJson.State.Health.Status)
+				return nil
+			},
+		},
+		{
+			Name:  "wait",
+			Usage: "Wait until a container enters Healthy status",
+			Action: func(c *cli.Context) error {
+				panic("Not Implemented!")
+			},
+		},
 	}
 	app.Run(os.Args)
+}
+
+func createClient() (*client.Client, error) {
+	defaultHeaders := map[string]string{"User-Agent": user_agent}
+	return client.NewClient("unix:///var/run/docker.sock", docker_api_version, nil, defaultHeaders)
 }
