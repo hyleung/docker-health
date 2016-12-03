831e5796b61a5e84e87be3704881f4490c37b843
Ho Yan Leung <hy.leung@gmail.com>
2016-12-03 07:44:07 -0800

    Add polling for container health w/timeout

--- wait.go
+++ wait.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
-	"github.com/docker/engine-api/types/events"
-	"github.com/docker/engine-api/types/filters"
 	"github.com/urfave/cli"
 	"golang.org/x/net/context"
-	"strings"
+	"time"
 )
 
 type WaitCommand struct {
@@ -22,6 +17,11 @@ func (*WaitCommand) Flags() []cli.Flag {
 		cli.BoolFlag{
 			Name:  "all, a",
 			Usage: "Wait on Healthcheck status for all running containers",
+		},
+		cli.Int64Flag{
+			Name:  "timeout",
+			Usage: "Wait timeout, in seconds",
+			Value: 60,
 		},
 	}
 }
@@ -38,48 +38,38 @@ func (*WaitCommand) Command() interface{} {
 			return cli.NewExitError("Ruh-roh!", 1)
 		}
 		containerName := c.Args().First()
-		return waitOnContainerHealth(docker_client, containerName)
+		return waitOnContainerHealth(docker_client, containerName, c.Int64("timeout"))
 	}
 }
 
-func waitOnContainerHealth(docker_client *client.Client, containerName string) error {
+func waitOnContainerHealth(docker_client *client.Client, containerName string, timeout int64) error {
 	log.Info(fmt.Sprintf("Waiting on health status of %s", containerName))
-	containerJson, err := docker_client.ContainerInspect(context.Background(), containerName)
-	if err != nil {
-		if client.IsErrContainerNotFound(err) {
-			return cli.NewExitError(fmt.Sprintf("Container %s not found", containerName), 1)
-		} else {
-			panic(err)
-		}
-	}
-	if containerJson.State.Health == nil {
-		//If the container doesn't have health checks, exit normally
-		log.Info(fmt.Sprintf("Container %s doesn't have any health checks defined", containerName))
-		return nil
-	} else if containerJson.State.Health.Status == "healthy" {
-		log.Info(fmt.Sprintf("Container %s is healthy", containerName))
-		//If the container is healthy, exit normally
-		return nil
-	} else {
-		//Keep checking
-		args := filters.NewArgs()
-		args.Add("Type", events.DaemonEventType)
-		readCloser, err := docker_client.Events(context.Background(), types.EventsOptions{Filters: args})
-		defer readCloser.Close()
+	startTime := time.Now().Unix()
+	for {
+
+		containerJson, err := docker_client.ContainerInspect(context.Background(), containerName)
 		if err != nil {
-			panic(err)
-		}
-		scanner := bufio.NewScanner(readCloser)
-		for scanner.Scan() {
-			var event events.Message
-			err = json.NewDecoder(strings.NewReader(scanner.Text())).Decode(&event)
-			action := event.Action
-			actor := event.Actor
-			if actor.Attributes["name"] == containerName {
-				log.Info("Action: %s, Actor: %s", action, actor)
+			if client.IsErrContainerNotFound(err) {
+				return cli.NewExitError(fmt.Sprintf("Container %s not found", containerName), 1)
+			} else {
+				panic(err)
 			}
-			return nil
 		}
-		return nil
+		if containerJson.State.Health == nil {
+			//If the container doesn't have health checks, exit normally
+			log.Info(fmt.Sprintf("Container %s doesn't have any health checks defined", containerName))
+			return nil
+		} else if containerJson.State.Health.Status == "healthy" {
+			log.Info(fmt.Sprintf("Container %s is healthy", containerName))
+			//If the container is healthy, exit normally
+			return nil
+		} else {
+			elapsed := time.Now().Unix() - startTime
+			if elapsed >= timeout {
+				return cli.NewExitError(fmt.Sprintf("Container %s failed to enter healthy state after %d seconds", containerName, timeout), 1)
+			}
+			//sleep
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
 }
